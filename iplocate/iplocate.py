e5a6bca575b44b6b322772cff789bea2394ea9a9
declercklouis <louis.declerck@student.howest.be>
2022-12-02 09:50:56 +0100

    finished iplocate.py

---
+++ iplocate/iplocate.py
@@ -0,0 +1,45 @@
+import requests
+import tabulate 
+
+def get_failed_ip():
+    log = open('/var/log/auth.log', 'r')
+    loglines = log.readlines()
+    iplist = []
+    for line in loglines:
+        line = line.strip()
+        if "Connection closed" in line and "[preauth]" in line:
+            port = line.split()[-2]
+            ip_address = line.split()[-4]
+            if ip_address not in iplist:
+                iplist.append(ip_address)
+        elif "Connection closed" in line and "[preauth]" not in line and "error" not in line:
+            # port = line.split()[-1]
+            ip_address = line.split()[-3]
+            if ip_address not in iplist:
+                iplist.append(ip_address)
+    return iplist
+
+def get_location(ip_address):
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    location_data = {
+        "ip": ip_address,
+        "city": response.get("city"),
+        "region": response.get("region"),
+        "country": response.get("country_name")
+    }
+    return location_data
+
+
+locationlist = []
+failed_iplist = get_failed_ip()
+for ip in failed_iplist:
+    iplocation = get_location(ip)
+    locationlist.append(iplocation)
+
+print(tabulate.tabulate(locationlist, headers="keys"))
+
+# for i in list:
+#     if i["country"] == "United States":
+#         print(i)
+        
+
