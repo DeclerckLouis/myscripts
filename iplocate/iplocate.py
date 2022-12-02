1ed317225836b4235aa2163ea9868f24d9a0c9ec
declercklouis <louis.declerck@student.howest.be>
2022-12-02 09:57:20 +0100

    added hostname and org to script

--- iplocate/iplocate.py
+++ iplocate/iplocate.py
@@ -26,6 +26,8 @@ def get_location(ip_address):
         "city": response.get("city"),
         "region": response.get("region"),
         "country": response.get("country_name")
+        "hostname": response.get("hostname")
+        "org": response.get("org")
     }
     return location_data
 
