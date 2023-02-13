04fa7b0b97370d40a1eb221282516a6c16ad7adf
declercklouis <louis.declerck@student.howest.be>
2023-02-13 14:43:54 +0100

    added more excercises and old scripts i had laying

---
+++ powershell/6_use_objects_properties.ps1
@@ -0,0 +1,6 @@
+#werken met objecten
+Get-Process # lijst van alle processen
+Get-Localuser # lijst van alle lokale gebruikers
+
+Get-Process | Get-Member # geeft alle properties van een proces weer
+
---
+++ powershell/7_get_help.ps1
@@ -0,0 +1,9 @@
+# learn how to work with help in powershell (get-help)
+
+Get-Help Start-Service -Full # geeft alle properties van een proces weer 
+
+Get-Help Start-Service -Examples # geeft de examples van start-service weer (als die er zijn)
+
+Get-Help Start-Service -Online # geeft de online help van start-service weer
+	
+Get-Help Start-Service -Parameter *Name* # geeft de parameters van start-service weer die Name bevatten (in dit geval is dat de enige parameter)
