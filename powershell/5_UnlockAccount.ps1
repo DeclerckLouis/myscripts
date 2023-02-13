27e92afb00f823debca871762c6a663530f6e33a
declercklouis <louis.declerck@student.howest.be>
2023-02-13 14:23:59 +0100

    added scripts i made during class

---
+++ powershell/5_UnlockAccount.ps1
@@ -0,0 +1,28 @@
+﻿#This script is what i used in SimLab1 to unlock accounts. It will be edited in the near future to use a GUI to select the account to unlock, instead of typing it in the console.
+
+Add-WindowsFeature RSAT-AD-Powershell
+Import-module ActiveDirectory
+cls
+
+#alle locked accounts opvragen + name en samaccountname afprinten
+write-Host "Alle locked accounts: "
+$lockedaccounts = Search-ADAccount -LockedOut | Select name,samaccountname
+$lockedaccounts
+write-Host "Einde locked accounts. (Indien hier niks staat, dan zijn er geen locked accounts.)"
+write-Host ""
+
+#één account unlocken, of niks unlocken.
+$yesorno = read-Host "Wilt u een account unlocken? (ja/nee)"
+
+while (1 -eq 1){
+    if($yesorno -eq "ja"){
+        $samaccountname = Read-Host "Geef een gebruikersnaam op om te unlocken"
+        Unlock-ADAccount $samaccountname
+        $yesorno = read-Host "Wilt u een account unlocken? (ja/nee)"
+    }
+    elseif($yesorno -eq "nee"){
+        break
+        #exit
+    }
+}
+
