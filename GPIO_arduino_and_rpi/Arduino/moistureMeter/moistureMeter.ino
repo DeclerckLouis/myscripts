f789dde3a78db627a6a1086974c2433c9aa96ab6
DeclerckLouis <Louis.declerck@student.howest.be>
2023-05-17 23:59:25 +0200

    code cleanup, should work now

--- GPIO_arduino_and_rpi/Arduino/moistureMeter/moistureMeter.ino
+++ GPIO_arduino_and_rpi/Arduino/moistureMeter/moistureMeter.ino
@@ -3,10 +3,10 @@
  * I thought about adding a green LED for when the levels are okay, but it would probably just annoy me. If you want to do it, just uncomment the part at 
  */
 
-int minThreshold = 100 // Change threshold as needed, it depends on the sensor, the soil and the plant.
-int maxThreshold = 500 // This is experimental, i might not use it.
-int ledWaterNeeded = 7;
-int delayValue = 500 // Can be changed if desired, personally, i don't see the point but go off
+int minThreshold = 100; // Change threshold as needed, it depends on the sensor, the soil and the plant.
+int maxThreshold = 500; // This is experimental, i might not use it.
+int ledWaterNeeded = 7; // LED on pin 7, can be changed if needed
+int delayValue = 500; // Can be changed if desired, personally, i don't see the point but go off
 
 
 void setup() {
@@ -23,44 +23,21 @@ void setup() {
 void loop() {
   int moistMeterValue = analogRead(A0); // read analog value every time the loop starts over
 
-  if (moistMeterValue < minThreshold){ //check moisture level against set threshold
+  if (moistMeterValue < minThreshold){ // lower than minThreshold?
     Serial.println("The soil is DRY (" + String(moistMeterValue));
     digitalWrite(ledWaterNeeded, HIGH);
   }
-  else if (minThreshold > moistMeterValue){
-    if (moistMeterValue < maxThreshold){
+  else if (minThreshold > moistMeterValue){ // higher than minThreshold?
+    if (moistMeterValue < maxThreshold){ // lower than maxThreshold? 
       Serial.println("The soil is GOOD (" + String(moistMeterValue)); 
   }
       
   }
-  else {
-    Serial.println("The soil is TOO WET (" + String(moistMeterValue));
-    blinkLED(ledWaterNeeded);
+  else { // higher than minThreshold and maxThreshold?
+    Serial.println("The soil is TOO WET (" + String(moistMeterValue)); 
   }
+  
   Serial.println(")");
   delay(delayValue);
   digitalWrite(ledWaterNeeded, LOW);
 }
-
-void blinkLED(int led){
-  digitalWrite(led, HIGH);
-  delay(200);
-  digitalWrite(led, LOW);
-  delay(200);
-  digitalWrite(led, HIGH);
-  delay(200);
-  digitalWrite(led, LOW);
-  delay(200);
-  digitalWrite(led, HIGH);
-  delay(200); // 1 sec
-  digitalWrite(led, LOW);
-  delay(200);
-  digitalWrite(led, HIGH);
-  delay(200);
-  digitalWrite(led, LOW);
-  delay(200);
-  digitalWrite(led, HIGH);
-  delay(200);
-  digitalWrite(led, LOW);
-  delay(200); // 2 sec
- }
