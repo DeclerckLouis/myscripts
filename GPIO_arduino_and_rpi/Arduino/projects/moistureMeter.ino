2565a3f2e92e0e4f651905748cff1830d204d39e
DeclerckLouis <Louis.declerck@student.howest.be>
2023-06-10 00:51:36 +0200

    moisturesebsor works

--- GPIO_arduino_and_rpi/Arduino/projects/moistureMeter.ino
+++ GPIO_arduino_and_rpi/Arduino/projects/moistureMeter.ino
@@ -1,43 +1,51 @@
-int sensor = 2; //touch sensor on pin 2
-int led = 7; // LED on pin 7, can be changed if needed
+/* This code is my adaptation of what i found on https://arduinogetstarted.com/tutorials/arduino-soil-moisture-sensor
+ * This prototype is supposed to check moisture levels of the soil, and notify the user when the levels are too low (LED)
+ * There are a lot of Serial.println's in this code, they are there for debugging purposes, and can be removed if desired
+ * The code is not perfect, but it works, and that's all that matters
+ */
 
-void setup()
-{
+const int delayValue = 100; // Can be changed if desired, personally, i don't see the point but go off
+const int wet = 184; // When soaked in water ## this value is based on my sensor
+const int dry = 500; // When dry ## this value is based on my sensor
+
+int blueLED = 7; // LED pin
+
+void setup() {
   Serial.begin(9600);
-  Serial.println("Ready for caregiving, delay set at 300.");
-  pinMode(led, OUTPUT);
-  pinMode(sensor, INPUT_PULLUP);
-  Serial.println("Blinking LED");
-  digitalWrite(led, HIGH);
-  delay(200);
-  digitalWrite(led, LOW);
-  delay(200);
-  digitalWrite(led, HIGH);
-  delay(200);
-  digitalWrite(led, LOW);
-  delay(200);
-  digitalWrite(led, HIGH);
-  delay(200);
-  digitalWrite(led, LOW);
-  delay(200);
-  digitalWrite(led, HIGH);
-  delay(200);
-  digitalWrite(led, LOW);
-  delay(200);
-  Serial.println("Initialization completed, awaiting input.");
+  Serial.println("Ready for caregiving, delay set at " + String(delayValue));
+  pinMode(blueLED, OUTPUT);
+  Serial.println("Blinking LED"); // Blink LED to show that the program is ready
+  blinkLED(blueLED);
+  blinkLED(blueLED);
+  }
+
+void loop() {
+  int moistValue = analogRead(A0); // Read analog sensor value ## i think this actually shows how many V A0 is getting?
+  int moistPercent = map(moistValue, wet, dry, 100, 0); //convert sensor value to percentage :) 
+  Serial.println("Current moisture percentage: " + String(moistPercent) + "%");
+
+  if (moistPercent < 30) {
+    Serial.println("Soil TOO DRY"); // If the soil is too dry, notify the user
+    digitalWrite(blueLED, HIGH);
+    }
+  else if (moistPercent > 80) {
+    Serial.println("soil TOO WET"); // If the soil is too wet, notify the user
+    blinkLED(blueLED); // this line can be commented out if the blinking is too annoying (just put less water dumbass)
+    }
+
+  else {
+    Serial.println("soil JUST GOOD"); // Dont's do anything
+    }
+  
+  delay(delayValue);
+  digitalWrite(blueLED, LOW); // Ensure LED is off when the program finishes, or when ELSE is triggered
 }
 
-void loop()
-{
-  if (digitalRead(sensor)){ // when sensor is pressed
-    Serial.println("YES TOUCH");
-    digitalWrite(led, HIGH);
-    delay(100);
-    digitalWrite(led, LOW);
-    delay(100);    
-    }
-  else {
-    Serial.println(".");
-    delay(100);
-    } 
-}
+void blinkLED(int led) { // Simple blink function, made it shorter so it's shorter, no problem since it's called every loop if the soil is too wet
+  digitalWrite(led, HIGH);
+  delay(200);
+  digitalWrite(led, LOW);
+  delay(200);
+  digitalWrite(led, HIGH);
+  delay(200);
+  }
