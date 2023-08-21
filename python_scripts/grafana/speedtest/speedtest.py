f82e32263162ef6bcd15e1f4e95b777a258b59b1
declercklouis <louis.declerck@student.howest.be>
2023-08-21 12:38:38 +0200

    Cleaned up code a little

--- python_scripts/grafana/speedtest/speedtest.py
+++ python_scripts/grafana/speedtest/speedtest.py
@@ -11,16 +11,16 @@ import datetime
 response = subprocess.Popen('/usr/bin/speedtest --accept-license --accept-gdpr', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
 
 # use regex to split it
-ping = re.search('Latency:\s+(.*?)\s', response, re.MULTILINE)
-download = re.search('Download:\s+(.*?)\s', response, re.MULTILINE)
-upload = re.search('Upload:\s+(.*?)\s', response, re.MULTILINE)
-jitter = re.search('Latency:.*?jitter:\s+(.*?)ms', response, re.MULTILINE)
+ping = re.search('Latency:\s+(.*?)\s', response, re.MULTILINE).group(1)
+download = re.search('Download:\s+(.*?)\s', response, re.MULTILINE).group(1)
+upload = re.search('Upload:\s+(.*?)\s', response, re.MULTILINE).group(1)
+jitter = re.search('Latency:.*?jitter:\s+(.*?)ms', response, re.MULTILINE).group(1)
 
-# a little more regex
-ping = ping.group(1)
-download = download.group(1)
-upload = upload.group(1)
-jitter = jitter.group(1)
+# Get the values
+# ping = ping.group(1)
+# download = download.group(1)
+# upload = upload.group(1)
+# jitter = jitter.group(1)
 
 
 # write it
