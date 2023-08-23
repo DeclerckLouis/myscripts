249d3a8e9d67ac94c280ec5e258915752cd786ca
declercklouis <louis.declerck@student.howest.be>
2023-08-23 20:27:25 +0200

    debugging made me lose my mind

--- python_scripts/grafana/iplocate/iplocate.py
+++ python_scripts/grafana/iplocate/iplocate.py
@@ -3,9 +3,11 @@ import requests
 import os
 
 #check if the save file exists, if not, create it and write the header
+# towrite = "/var/www/apache/failedip/failedip.csv"
+towrite = "failedip.csv"
 
-with open('/var/www/apache/failedip/failedip.csv', 'a+') as csvfile:
-        if os.stat('/var/www/apache/failedip/failedip.csv').st_size == 0:
+with open(towrite, 'a+') as csvfile:
+        if os.stat(towrite).st_size == 0:
             print("file is empty")
             csvfile.write("timestamp,ip\r\n")
             with open('/var/log/auth.log', 'r') as log:
@@ -27,7 +29,7 @@ with open('/var/www/apache/failedip/failedip.csv', 'a+') as csvfile:
 
         else:
             print("file is not empty")
-            with open('/var/www/apache/failedip/failedip.csv', 'r') as csvfile:
+            with open(towrite, 'r') as csvfile:
                 last_line = csvfile.readlines()[-1]
                 lastlog = last_line.split(",")[0]
                 print(lastlog)
@@ -50,7 +52,7 @@ with open('/var/www/apache/failedip/failedip.csv', 'a+') as csvfile:
                             if ip_address not in iplist:
                                 iplist.append([logtime, ip_address])
                         
-with open('/var/www/apache/failedip/failedip.csv', 'a+') as csvfile:
+with open(towrite, 'a+') as csvfile:
     for i in iplist:
         csvfile.write(str(i[0]) + "," + i[1] + "\r\n")
 
