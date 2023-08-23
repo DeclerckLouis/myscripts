dfe20183db774603febe876880478aafeea299c0
declercklouis <louis.declerck@student.howest.be>
2023-08-23 20:08:05 +0200

    iplocate for grafana, not quite finished yet

---
+++ python_scripts/grafana/iplocate/iplocate.py
@@ -0,0 +1,79 @@
+import datetime
+import requests
+import os
+
+#check if the save file exists, if not, create it and write the header
+
+with open('/var/www/apache/failedip/failedip.csv', 'a+') as csvfile:
+        if os.stat('/var/www/apache/failedip/failedip.csv').st_size == 0:
+            print("file is empty")
+            csvfile.write("timestamp,ip\r\n")
+            with open('/var/log/auth.log', 'r') as log:
+                #start reading the log file  
+                lines = log.readlines()
+                iplist = []
+                for l in lines:
+                    l=l.strip()
+                    #check if the line contains the string "invalid user" or "authenticating user" and "[preauth]"
+                    if ("invalid user" in l or "authenicating user" in l) and "[preauth]" in l:
+                        ip_address = l.split()[-4]
+                        month = l.split()[0]
+                        day = l.split()[1]
+                        time = l.split()[2]
+                        year = datetime.datetime.now().year
+                        logtime = datetime.datetime.strptime(day + " " + month + " " + str(year) + " " + time, "%d %b %Y %H:%M:%S")
+                        if ip_address not in iplist:
+                            iplist.append([logtime, ip_address])
+
+        else:
+            print("file is not empty")
+            with open('/var/www/apache/failedip/failedip.csv', 'r') as csvfile:
+                last_line = csvfile.readlines()[-1]
+                lastlog = last_line.split(",")[0]
+                print(lastlog)
+            with open('/var/log/auth.log', 'r') as log:
+                #start reading the log file  
+                lines = log.readlines()
+                iplist = []
+                for l in lines:
+                    l=l.strip()
+                    if l.startswith(lastlog):
+                        print("found last log")
+                    #check if the line contains the string "invalid user" or "authenticating user" and "[preauth]"
+                        if ("from invalid user" in l or "by invalid user" or "authenicating user" in l) and "[preauth]" in l:
+                            ip_address = l.split()[-4]
+                            month = l.split()[0]
+                            day = l.split()[1]
+                            time = l.split()[2]
+                            year = datetime.datetime.now().year
+                            logtime = datetime.datetime.strptime(day + " " + month + " " + str(year) + " " + time, "%d %b %Y %H:%M:%S")
+                            if ip_address not in iplist:
+                                iplist.append([logtime, ip_address])
+                        
+with open('/var/www/apache/failedip/failedip.csv', 'a+') as csvfile:
+    for i in iplist:
+        csvfile.write(str(i[0]) + "," + i[1] + "\r\n")
+
+
+
+
+# with open('/var/log/auth.log', 'r') as log:
+#     #start reading the log file  
+#     lines = log.readlines()
+#     iplist = []
+#     for l in lines:
+#         l=l.strip()
+#         if l.startswith(lastlog):
+#             print("found last log")
+#             #check if the line contains the string "invalid user" or "authenticating user" and "[preauth]"
+#             if ("invalid user" in l or "authenicating user" in l) and "[preauth]" in l:
+#                 ip_address = l.split()[-4]
+#                 month = l.split()[0]
+#                 day = l.split()[1]
+#                 time = l.split()[2]
+#                 year = datetime.datetime.now().year
+#                 logtime = datetime.datetime.strptime(day + " " + month + " " + str(year) + " " + time, "%d %b %Y %H:%M:%S")
+#                 if ip_address not in iplist:
+#                     iplist.append([logtime, ip_address])
+
+
